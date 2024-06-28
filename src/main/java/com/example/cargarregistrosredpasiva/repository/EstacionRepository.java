d1e1d6db939e9358573d49308900ec4152b619ff
andresigacgmail <andres.leal.igac@gmail.com>
2024-06-28 01:07:44 -0500

    fix update geom

--- src/main/java/com/example/cargarregistrosredpasiva/repository/EstacionRepository.java
+++ src/main/java/com/example/cargarregistrosredpasiva/repository/EstacionRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EstacionRepository extends JpaRepository<Estacion, Integer> {
 
-    @Query(value = "insert into main_estacion (identificador, nomenclatura, estado_vertice,  municipio, latitud, longitud, altura_elipsoidal, agencia, geom) values (?,?, ?,?, ?, ?,?, ?, ST_SetSRID(ST_MakePoint(?, ?), 9377)) returning *", nativeQuery = true)
+    @Query(value = "insert into main_estacion (identificador, nomenclatura, estado_vertice,  municipio, latitud, longitud, altura_elipsoidal, agencia, geom) values (?,?, ?,?, ?, ?,?, ?, ST_Transform(ST_SetSRID(ST_MakePoint(?, ?), 4326), 9377) returning *", nativeQuery = true)
     Estacion saveEstacion(String identificador, String nomenclatura, int estadoVertice, int idMunicipio, double latitud, double longitud, double alturaElipsoidal,  int agencia, double lon, double lat);
 }
